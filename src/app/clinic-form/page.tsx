adae7a62d4c3a9cbcaa1a9d46764c5bfcf147a0c
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2025-05-27 21:47:31 -0300

    feat: add clinic form page

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import ClinicForm from "./components/form";
+import z from "zod";
+
+const ClinicFormSchema = z.object({
+  name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+});
+
+const ClinicFormPage = () => {
+  return (
+    <div>
+      <Dialog open>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Adicionar clínica</DialogTitle>
+            <DialogDescription>
+              Preencha o formulário abaixo para adicionar uma clínica.
+            </DialogDescription>
+          </DialogHeader>
+          <ClinicForm />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ClinicFormPage;
